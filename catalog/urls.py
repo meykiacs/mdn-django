b204ab6a493068c4a3fa3d60ae7cd3e66c4bb09c
Mey Kia <meykiacs@gmail.com>
2022-08-08 01:20:46 +0430

    Part 5 inished: Creating our home page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-
-]
+urlpatterns = [path("", views.index, name="index")]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+    # Generate counts of some of the main objects
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    num_generes_contain_g = Genre.objects.filter(name__icontains='g').count()
+    num_books_contain_b = Book.objects.filter(title__icontains='b').count()
+    
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_generes_contain_g': num_generes_contain_g,
+        'num_books_contain_b': num_books_contain_b,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
